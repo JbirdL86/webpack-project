0ef7e6748f0e48a43131e0419c2ac6a1092aa124
JbirdL86 <jgonzalez.wj@gmail.com>
2021-07-28 05:44:41 -0600

    Add drag and drop feature

---
+++ src/completed.js
@@ -0,0 +1,7 @@
+//const checkBox = li.querySelector('.checkbox');
+
+function checkCompleted (e,li) {
+  
+}
+
+export default checkCompleted;
---
+++ src/storage.js
@@ -0,0 +1,28 @@
+class Storage {
+  setStorage(taskListgit ) {
+    window.localStorage.setItem('tasks', JSON.stringify(taskList));
+  }
+
+  getStorage() {
+    const listStorage = JSON.parse(window.localStorage.getItem('tasks'));
+    const taskList = listStorage.map((task) => {
+      const newTask = {
+        description: task.description,
+        completed: task.completed,
+        index: task.index,
+      }
+    });
+    return taskList;
+  }
+
+  hasStorage() {
+    if (window.localStorage.getItem('tasks') !== null && window.localStorage.getItem('tasks') > 0 ) {
+      return true;
+    }
+    return false;
+  }
+}
+
+const storage = new Storage();
+
+export {storage as default};
