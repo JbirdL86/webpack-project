96e6087f0b68b3ab4e7d7a1d55446bbece787347
JbirdL86 <jgonzalez.wj@gmail.com>
2021-07-31 08:47:25 -0600

    Fix linter erros and move functions to crud file

--- src/index.js
+++ src/index.js
@@ -2,28 +2,17 @@
 import _, { create } from 'lodash';
 import './style.css';
 import setStorage from './storage.js';
-import { taskList, displayTasks, setListners } from './crud';
+import { taskList, displayTasks, setListners } from './crud.js';
 
 let tasksList = taskList;
 
-
-
-
-
-
-
 if (localStorage.getItem('tasks')) {
   tasksList = JSON.parse(localStorage.getItem('tasks'));
+
   displayTasks(tasksList);
   setListners();
-  console.log(tasksList.length)
-  console.log(taskList)
-
-
 } else {
   setStorage(tasksList);
   displayTasks(tasksList);
   setListners();
-  // console.log(tasksList.pop())
-
 }
